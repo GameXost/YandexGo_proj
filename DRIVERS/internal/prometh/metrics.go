cf9edecc725588ca9bc20c2bfd48c8b4b94e243b
GameXost <dsergey010206@gmail.com>
2025-05-26 21:46:52 +0300

    shit

--- DRIVERS/internal/prometh/metrics.go
+++ DRIVERS/internal/prometh/metrics.go
@@ -1,33 +1,33 @@
 package prometh
 
 import (
-    "github.com/prometheus/client_golang/prometheus"
-    "github.com/prometheus/client_golang/prometheus/promauto"
-    "github.com/prometheus/client_golang/prometheus/promhttp"
-    "net/http"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"net/http"
 )
 
 var (
-    RideAcceptedCounter = promauto.NewCounter(prometheus.CounterOpts{
-        Name: "driver_ride_accepted_total",
-        Help: "Total number of accepted rides",
-    })
+	RideAcceptedCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "driver_ride_accepted_total",
+		Help: "Total number of accepted rides",
+	})
 
-    RideCompletedCounter = promauto.NewCounter(prometheus.CounterOpts{
-        Name: "driver_ride_completed_total",
-        Help: "Total number of completed rides",
-    })
+	RideCompletedCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "driver_ride_completed_total",
+		Help: "Total number of completed rides",
+	})
 
-    RideCanceledCounter = promauto.NewCounter(prometheus.CounterOpts{
-        Name: "driver_ride_canceled_total",
-        Help: "Total number of canceled rides",
-    })
+	RideCanceledCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "driver_ride_canceled_total",
+		Help: "Total number of canceled rides",
+	})
 )
 
 // InitPrometheus запускает endpoint /prometh
 func InitPrometheus(addr string) {
-    go func() {
-        http.Handle("/metrics", promhttp.Handler())
-        _ = http.ListenAndServe(addr, nil)
-    }()
+	go func() {
+		http.Handle("/metrics", promhttp.Handler())
+		_ = http.ListenAndServe(addr, nil)
+	}()
 }
